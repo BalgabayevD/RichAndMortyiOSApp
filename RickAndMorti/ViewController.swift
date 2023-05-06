2dec5aada7cf58509b6785ca7ff4f21d7e65843b
DBalgabayev <DBalgabayev@beeline.kz>
2023-05-06 19:54:05 +0600

    Initial Commit

---
+++ RickAndMorti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorti
+//
+//  Created by Damir Balgabayev on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
